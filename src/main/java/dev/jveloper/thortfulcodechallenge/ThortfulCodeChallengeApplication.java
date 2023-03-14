3d36c5b44039303300fd20a58ff9c1c927b34412
jveloper <jveloper82@gmail.com>
2023-03-14 16:09:22 +0000

    initial commit with basic application structure

---
+++ src/main/java/dev/jveloper/thortfulcodechallenge/ThortfulCodeChallengeApplication.java
@@ -0,0 +1,13 @@
+package dev.jveloper.thortfulcodechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThortfulCodeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThortfulCodeChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jveloper/thortfulcodechallenge/ThortfulCodeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jveloper.thortfulcodechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThortfulCodeChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
