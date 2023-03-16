6329ead39f54be60c7523a602e1b5a19fb3015e7
jveloper <jveloper82@gmail.com>
2023-03-16 11:00:49 +0000

    add configurations classes

--- src/main/java/dev/jveloper/thortfulcodechallenge/configs/WebClientConfig.java
+++ src/main/java/dev/jveloper/thortfulcodechallenge/configs/WebClientConfig.java
@@ -1,19 +1,34 @@
 package dev.jveloper.thortfulcodechallenge.configs;
 
+import lombok.extern.java.Log;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
+@Log
 public class WebClientConfig {
 
     @Bean
     WebClient webClient(){
 
         return WebClient.builder()
-                .baseUrl("https://randomuser.me/api/")
+                .baseUrl("https://dummyjson.com/")
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.newConnection().compress(true).wiretap(true)))
+                .filter(logRequest())
                 .build();
+    }
+
+    private static ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            log.info(String.valueOf(clientRequest.url()));
+            return Mono.just(clientRequest);
+        });
     }
 
 }
