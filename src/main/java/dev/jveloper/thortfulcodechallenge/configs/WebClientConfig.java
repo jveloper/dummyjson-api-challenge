2b0d37f530aabdc106aa552534dbe6ada1567182
jveloper <jveloper82@gmail.com>
2023-03-16 15:23:17 +0000

    remove unneeded line from config webclient

--- src/main/java/dev/jveloper/thortfulcodechallenge/configs/WebClientConfig.java
+++ src/main/java/dev/jveloper/thortfulcodechallenge/configs/WebClientConfig.java
@@ -3,12 +3,9 @@ package dev.jveloper.thortfulcodechallenge.configs;
 import lombok.extern.java.Log;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
 
 @Configuration
 @Log
@@ -19,7 +16,6 @@ public class WebClientConfig {
 
         return WebClient.builder()
                 .baseUrl("https://dummyjson.com/")
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.newConnection().compress(true).wiretap(true)))
                 .filter(logRequest())
                 .build();
     }
