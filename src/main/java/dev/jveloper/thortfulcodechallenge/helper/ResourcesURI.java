7aea3939bbb8636ea5a4c869ab1420edcb3feea7
jveloper <jveloper82@gmail.com>
2023-03-16 15:26:21 +0000

    add nested user url path

--- src/main/java/dev/jveloper/thortfulcodechallenge/helper/ResourcesURI.java
+++ src/main/java/dev/jveloper/thortfulcodechallenge/helper/ResourcesURI.java
@@ -5,8 +5,9 @@ import lombok.Getter;
 @Getter
 public class ResourcesURI {
 
-    public final static String URI_CARTS = "carts/";
-    public final static String URI_USERS = "users/";
-    public final static String URI_PRODUCTS = "products/";
+    public final static String URI_CARTS = "carts";
+    public final static String URI_USERS = "users";
+    public final static String URI_NESTED_USER = "/user/";
+    public final static String URI_PRODUCTS = "products";
 
 }
