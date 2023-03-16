15d555348244ee7d75b006d288f145e147722e7e
jveloper <jveloper82@gmail.com>
2023-03-16 22:35:51 +0000

    add uri content

--- src/main/java/dev/jveloper/thortfulcodechallenge/helper/ResourcesURI.java
+++ src/main/java/dev/jveloper/thortfulcodechallenge/helper/ResourcesURI.java
@@ -5,9 +5,17 @@ import lombok.Getter;
 @Getter
 public class ResourcesURI {
 
-    public final static String URI_CARTS = "carts";
-    public final static String URI_USERS = "users";
+
+    public final static String URI_USERS_LIST = "users";
+    public final static String URI_USERS = "users/";
     public final static String URI_NESTED_USER = "/user/";
-    public final static String URI_PRODUCTS = "products";
+    public final static String URI_ADD_USER = "users/add";
+
+    public final static String URI_CARTS_LIST = "carts";
+    public final static String URI_CARTS = "carts/";
+
+    public final static String URI_PRODUCTS_LIST = "products";
+    public final static String URI_PRODUCTS = "products/";
+
 
 }
